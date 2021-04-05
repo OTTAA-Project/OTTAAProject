6409542ff2487b466424f6119f1c1a13a438e971
gonojuarez <gonojuarez17@gmail.com>
2021-04-05 14:39:52 -0300

    we created the Junit testing package in android  and there is running In a debug branch

---
+++ app/src/test/java/com/stonefacesoft/ottaa/test/Components/Preferences.java
@@ -0,0 +1,37 @@
+package com.stonefacesoft.ottaa.test.Components;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+import androidx.preference.PreferenceManager;
+
+public class Preferences {
+  private   SharedPreferences preferences;
+    public Preferences(Context mContext){
+        preferences= PreferenceManager.getDefaultSharedPreferences(mContext);
+    }
+
+    public void editBoolean(String name,Boolean value){
+        preferences.edit().putBoolean(name,value).apply();
+    }
+
+    public void editString(String name,String value){
+        preferences.edit().putString(name,value).apply();
+    }
+
+    public void editInteger(String name,int value){
+        preferences.edit().putInt(name,value).apply();
+    }
+
+    public void editFloat(String name,float value){
+        preferences.edit().putFloat(name,value);
+    }
+
+    public void editLong(String name,long value){
+        preferences.edit().putLong(name,value);
+    }
+
+    public SharedPreferences getPreferences() {
+        return preferences;
+    }
+}
---
+++ app/src/test/java/com/stonefacesoft/ottaa/test/JUnitSuiteClasses.java
@@ -0,0 +1,10 @@
+package com.stonefacesoft.ottaa.test;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({  UnitTestingScore.class})
+public class JUnitSuiteClasses {
+
+}
