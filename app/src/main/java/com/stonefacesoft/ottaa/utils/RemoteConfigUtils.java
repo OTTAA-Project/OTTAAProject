84c7d5ec3fafc89b503caff1218f0429ce33a688
gonojuarez <gonojuarez17@gmail.com>
2021-06-09 13:49:07 -0300

    Remote Config for the price

---
+++ app/src/main/java/com/stonefacesoft/ottaa/utils/RemoteConfigUtils.java
@@ -0,0 +1,78 @@
+package com.stonefacesoft.ottaa.utils;
+
+import android.app.Activity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.firebase.remoteconfig.FirebaseRemoteConfig;
+import com.google.firebase.remoteconfig.FirebaseRemoteConfigSettings;
+import com.stonefacesoft.ottaa.Interfaces.RemoteConfigListener;
+
+/**
+ * This method should be an singleton classes
+ */
+
+public class RemoteConfigUtils implements RemoteConfigListener.PriceListener {
+    private static RemoteConfigUtils remoteConfigUtils;
+    private final FirebaseRemoteConfig mFirebaseRemoteConfig;
+    private FirebaseRemoteConfigSettings mfirebaseRemoteConfigSettings;
+
+
+    private RemoteConfigUtils() {
+        mFirebaseRemoteConfig = FirebaseRemoteConfig.getInstance();
+        FirebaseRemoteConfigSettings configSettings = new FirebaseRemoteConfigSettings.Builder()
+                .setMinimumFetchIntervalInSeconds(0)
+                .build();
+        mFirebaseRemoteConfig.setConfigSettingsAsync(configSettings);
+    }
+
+    public synchronized static RemoteConfigUtils getInstance() {
+        if (remoteConfigUtils == null) remoteConfigUtils = new RemoteConfigUtils();
+        return remoteConfigUtils;
+    }
+
+    public RemoteConfigUtils setDefaultParameter(int value) {
+        mFirebaseRemoteConfig.setDefaultsAsync(value);
+        return this;
+    }
+
+    public void activateFetched() {
+        mFirebaseRemoteConfig.fetch().addOnCompleteListener(task -> {
+            if (task.isSuccessful()) {
+                mFirebaseRemoteConfig.fetchAndActivate();
+            }
+        }).addOnFailureListener(e -> {
+
+        });
+    }
+
+    public void setActivateDeactivateConfig(Activity mActivity, OnCompleteListener onCompleteListener) {
+        mFirebaseRemoteConfig.fetchAndActivate().addOnCompleteListener(mActivity, onCompleteListener);
+    }
+
+
+    @Override
+    public boolean isRegionPriceEnabled() {
+        return mFirebaseRemoteConfig.getBoolean("user_price_premium");
+    }
+
+    @Override
+    public String changePrice(String text, String name) {
+        if (text.contains("990 ARS"))
+            return text.replace("990 ARS", mFirebaseRemoteConfig.getString(name));
+        if (text.contains("9 USD"))
+            return text.replace("9 USD", mFirebaseRemoteConfig.getString(name));
+        if (text.contains("9 دولارات أمريكية"))
+            return text.replace("9 دولارات أمريكية", mFirebaseRemoteConfig.getString(name));
+        return text.replace("9 USD", mFirebaseRemoteConfig.getString(name));
+    }
+
+
+    @Override
+    public String paymentUtri() {
+        return mFirebaseRemoteConfig.getString("payment_uri");
+    }
+    @Override
+    public String paymentUtriPremium() {
+        return mFirebaseRemoteConfig.getString("payment_uri_premium");
+    }
+}
