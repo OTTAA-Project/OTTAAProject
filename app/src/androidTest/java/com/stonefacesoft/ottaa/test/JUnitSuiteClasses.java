08354fcffca87608e52d5df3c9630cbae8b30b81
gonojuarez <gonojuarez17@gmail.com>
2021-08-09 11:13:14 -0300

    Testing update

--- app/src/androidTest/java/com/stonefacesoft/ottaa/test/JUnitSuiteClasses.java
+++ app/src/androidTest/java/com/stonefacesoft/ottaa/test/JUnitSuiteClasses.java
@@ -1,11 +1,9 @@
 package com.stonefacesoft.ottaa.test;
 
 import com.stonefacesoft.ottaa.test.unitTesting.UnitTestingConnectionDetector;
-import com.stonefacesoft.ottaa.test.unitTesting.UnitTestingCreatePictograms;
 import com.stonefacesoft.ottaa.test.unitTesting.UnitTestingFavoritePhrases;
 import com.stonefacesoft.ottaa.test.unitTesting.UnitTestingGame;
 import com.stonefacesoft.ottaa.test.unitTesting.UnitTestingGetPictogramsById;
-import com.stonefacesoft.ottaa.test.unitTesting.UnitTestingGoogleTranslate;
 import com.stonefacesoft.ottaa.test.unitTesting.UnitTestingGroups;
 import com.stonefacesoft.ottaa.test.unitTesting.UnitTestingJson;
 import com.stonefacesoft.ottaa.test.unitTesting.UnitTestingPhrases;
@@ -16,7 +14,7 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
 @RunWith(Suite.class)
-@Suite.SuiteClasses({UnitTestingConnectionDetector.class, UnitTestingCreatePictograms.class, UnitTestingFavoritePhrases.class, UnitTestingGame.class, UnitTestingGetPictogramsById.class, UnitTestingGroups.class, UnitTestingJson.class, UnitTestingPhrases.class, UnitTestingPremiumUser.class, UnitTestingScore.class, UnitTestingGoogleTranslate.class})
+@Suite.SuiteClasses({UnitTestingConnectionDetector.class, UnitTestingCreatePictograms.class, UnitTestingFavoritePhrases.class, UnitTestingGame.class, UnitTestingGetPictogramsById.class, UnitTestingGroups.class, UnitTestingJson.class, UnitTestingPhrases.class, UnitTestingPremiumUser.class, UnitTestingScore.class})
 public class JUnitSuiteClasses {
  public static int testRunning;
 }
