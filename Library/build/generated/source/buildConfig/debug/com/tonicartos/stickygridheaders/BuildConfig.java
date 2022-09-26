744e76bc1f0d6ecea74a25a3b5344ac11f689a9f
Hector <hcosta@ottaaproject.com>
2022-09-26 16:28:30 -0300

    v98 in Production

---
+++ Library/build/generated/source/buildConfig/debug/com/tonicartos/stickygridheaders/BuildConfig.java
@@ -0,0 +1,10 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.tonicartos.stickygridheaders;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String LIBRARY_PACKAGE_NAME = "com.tonicartos.stickygridheaders";
+  public static final String BUILD_TYPE = "debug";
+}
