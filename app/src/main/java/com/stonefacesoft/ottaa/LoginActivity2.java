7bcccc7540135d1bcc4346a403fa8eb4f0696863
gonojuarez <gonojuarez17@gmail.com>
2023-03-08 14:00:47 -0300

    Login activity context validation

--- app/src/main/java/com/stonefacesoft/ottaa/LoginActivity2.java
+++ app/src/main/java/com/stonefacesoft/ottaa/LoginActivity2.java
@@ -52,6 +52,7 @@ import com.stonefacesoft.ottaa.utils.InmersiveMode;
 import com.stonefacesoft.ottaa.utils.IntentCode;
 import com.stonefacesoft.ottaa.utils.ObservableInteger;
 import com.stonefacesoft.ottaa.utils.RemoteConfigUtils;
+import com.stonefacesoft.pictogramslibrary.utils.ValidateContext;
 
 import java.io.File;
 import java.util.Locale;
@@ -419,7 +420,7 @@ public class LoginActivity2 extends AppCompatActivity implements View.OnClickLis
     public class BackgroundTask extends AsyncTask<Void, Void, Void> {
         @Override
         protected void onPreExecute() {
-            if (!LoginActivity2.this.isFinishing()) {
+            if (ValidateContext.isValidContext(LoginActivity2.this)) {
                 dialog.setMessage(getResources().getString(R.string.pref_login_wait));
                 dialog.setTitle(getResources().getString(R.string.pref_login_autentificando));
                 dialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
